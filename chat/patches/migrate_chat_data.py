953d7e9d1a8b4be634a39b623787ac6f8c489c4e
jeowsome <62892395+jeowsome@users.noreply.github.com>
2024-04-25 21:29:47 +0800

    Update migrate_chat_data.py

--- chat/patches/migrate_chat_data.py
+++ chat/patches/migrate_chat_data.py
@@ -11,6 +11,11 @@ def execute():
     migrate_messages()
 
 
+def change_column_type():
+    frappe.db.change_column("Chat Message", "content", "Long Text")
+    frappe.db.commit()
+
+
 def migrate_rooms():
     old_rooms_list = frappe.qb.from_(
         'Chat Room').select('*').run(as_dict=True)
